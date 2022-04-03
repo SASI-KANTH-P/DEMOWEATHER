da65161b94964020b1984a44045ccbd7af996006
SASI-KANTH-P <sasikanthsk002@gmail.com>
2022-04-03 13:19:55 +0530

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+import searchInput from './components/searchInput';
+import getImageWeather from './utils/getImageWeather';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    handleUpdateLocation = async city =>{
+      if(!city) return ;
+      this.setSate({loading: true}, async () =>{
+        try
+        { const locationId= await fetchLocationId(city);
+          const {location,weather,temperature} = await fetchWeather(locationId,weather,temperature);
+        }
+        finally
+        {
+          
+        }
+      }
+      )
+    }
+
+    
+      )
+
+  
 }
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <ImageBackground
+    source={getImageWeather(weather)}
+    style={styles.imageContainer}
+    imageStyle={styles.image}>
+    <View style={styles.container}>
+      <Text style={[styles.textStyle,styles.largeText]}>SHANGHAI</Text>
+      <Text style={[styles.smallText,styles.textStyle]}>Light Cloud</Text>
+      <Text style={[styles.smallText,styles.textStyle]}>23 Degrees</Text>
+    <searchInput placeholder="search any city" onSubmit={this.handleUpdateLocation}/>
+    </View>
+    </ImageBackground>
+    </KeyboardAvoidingView>
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +46,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    textAlign:'center',
+    fontFamily:Platform.OS == 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText:
+  {
+    fontSize: 45,
+  },
+
+  smallText:{
+    fontSize:20,
+  },
+
+  textInput:
+  {
+  backgroundColor:'blue',
+  color:'black',
+  height:45,
+  width:300,
+        backgroundColor:'#666',
+        color:'black',
+        marginTop:25,
+        marginHorizontal:20,
+        paddingHorizontal:10,
+        alignSelf:'center',
+  }
 });
